c57e9570e6d8e0e8c753447ed41fc2888090449e
u20241222301@usco.edu.co <u20241222301@usco.edu.co>
2025-11-04 08:17:59 -0500

    actualizacion

--- src/main/java/com/example/parcial2pw/controller/LoginController.java
+++ src/main/java/com/example/parcial2pw/controller/LoginController.java
@@ -15,20 +15,5 @@ public class LoginController {
         return "login"; // login.html
     }
 
-    @GetMapping("/home")
-    public String home() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String role = auth.getAuthorities().iterator().next().getAuthority();
-
-        switch (role) {
-            case "ROLE_RECTOR":
-                return "redirect:/rector/home";
-            case "ROLE_DOCENTE":
-                return "redirect:/docente/home";
-            case "ROLE_ESTUDIANTE":
-                return "redirect:/estudiante/home";
-            default:
-                return "redirect:/login?error";
-        }
-    }
+  
 }
